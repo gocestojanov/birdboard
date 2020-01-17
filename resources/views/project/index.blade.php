7470b9929af4e3a441e3cbc672909d33b9aab680
goce <goce@debian.laravel.local>
2020-01-17 08:48:55 -0500

    Episode 9 - Projects Gadget Tailwind

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -3,17 +3,30 @@
 @section('content')
 
     <div class="flex items-center">
-
-
         <a href="/projects/create" class="mr-auto mb-4">Create a new Project</a>
     </div>
 
-    <ul>
+
+
+
+
+<div class="flex">
     @forelse ($projects as $project)
-        <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+
+
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" >
+                <h3 class="font-normal text-xl py-4"><a href="{{ $project->path() }}">{{ $project->title }}</a></h3>
+
+                <div class="text-gray-500">
+                    {{ Str::limit($project->description,150) }}
+                </div>
+
+        </div>
+
+
     @empty
-        <h1>No Projects Jet</h1>
+        <div>No Projects Jet</div>
     @endforelse
-    </ul>
+</div>
 
 @endsection
